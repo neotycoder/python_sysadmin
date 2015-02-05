f7fcaa8f09071845d343550dff22f882bcf9ff42
neotycoder <neotycoder@users.noreply.github.com>
2015-02-05 13:49:28 -0800

    Windows version of createDirectory.py
    Added a windows version.

---
+++ createDirectoryWindows.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+
+# Author: Ty Lim
+# Date: 2/5/2015
+# File name: createDirectory.py
+# Python Version: 2.X, Windows
+# Description: This script shows the example of how concatenation works in python, utilizing the os module to check a directory
+# and create it if it does not exist, and opening and closing a file for writing.
+#
+# This is nothing too difficult, and you can certainly add more functionality to it if you wish.
+# One idea is to build this whole example into an object with subsequent methods.
+
+import os, datetime
+
+# Create the file name
+baseFileName = "12345"
+app="someAppName"
+dateTimeStamp = datetime.date.today()
+# This directory needs to exists. It is the parent directory that will house the datafiles.
+targetDirectory = "C:\datafiles"
+targetfileName = str(baseFileName)+"_"+app+"."+str(dateTimeStamp)+".txt"
+fullPathToFile = targetDirectory+"\\" + targetfileName
+
+
+print "FileName: " + fullPathToFile
+
+# Check base path. If it does not exist, create it.
+if not os.path.isdir(targetDirectory):
+    os.makedirs(targetDirectory)
+
+file = open(fullPathToFile,'w')
+file.write("This is a test.")
